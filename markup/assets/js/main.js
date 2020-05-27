b3c84963fbfc40b67245cb39afb8c4fbe421f126
Igor Gorlov <gorlov35@gmail.com>
2020-05-27 23:55:35 +0300

    fix popup exists js error on main page

--- markup/assets/js/main.js
+++ markup/assets/js/main.js
@@ -2,6 +2,8 @@ import svg4everybody from 'svg4everybody';
 import exhibitionSlider from '../../blocks/exhibition/exhibition';
 import aboutSlider from '../../blocks/about/about';
 import featuredSlider from '../../blocks/featured/featured';
+import popup from '../../blocks/popup/popup';
+
 import Tabby from 'tabbyjs';
 
 // import levelsSlider from '../../blocks/levels/levels';
@@ -187,35 +189,35 @@ import Tabby from 'tabbyjs';
 
     // Popup
 
+    popup();
 
-    $('.open-popup').magnificPopup({
-      type: 'inline'
-    });
-
+    // $('.open-popup').magnificPopup({
+    //   type: 'inline'
+    // });
 
 
 
     // List pages
 
-    // function pageWidget(pages) {
-    //   const widgetWrap = $('<div class="widget_wrap"><ul class="widget_list"></ul></div>');
-    //   widgetWrap.prependTo("body");
-    //   for (let i = 0; i < pages.length; i++) {
-    //     $('<li class="widget_item"><a class="widget_link" href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').appendTo('.widget_list');
-    //   }
-    //   const widgetStilization = $('<style>body {position:relative} .widget_wrap{position:absolute;top:0;left:0;z-index:9999;padding:10px 20px;background:#222;border-bottom-right-radius:10px;-webkit-transition:all .3s ease;transition:all .3s ease;-webkit-transform:translate(-100%,0);-ms-transform:translate(-100%,0);transform:translate(-100%,0)}.widget_wrap:after{content:" ";position:absolute;top:0;left:100%;width:24px;height:24px;background:#222 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAAAxQTFRF////////AAAA////BQBkwgAAAAN0Uk5TxMMAjAd+zwAAACNJREFUCNdjqP///y/DfyBg+LVq1Xoo8W8/CkFYAmwA0Kg/AFcANT5fe7l4AAAAAElFTkSuQmCC) no-repeat 50% 50%;cursor:pointer}.widget_wrap:hover{-webkit-transform:translate(0,0);-ms-transform:translate(0,0);transform:translate(0,0)}.widget_item{padding:0 0 10px}.widget_link{color:#fff;text-decoration:none;font-size:15px;}.widget_link:hover{text-decoration:underline} </style>');
-    //   widgetStilization.prependTo(".widget_wrap");
-    // }
+    function pageWidget(pages) {
+      const widgetWrap = $('<div class="widget_wrap"><ul class="widget_list"></ul></div>');
+      widgetWrap.prependTo("body");
+      for (let i = 0; i < pages.length; i++) {
+        $('<li class="widget_item"><a class="widget_link" href="' + pages[i] + '.html' + '">' + pages[i] + '</a></li>').appendTo('.widget_list');
+      }
+      const widgetStilization = $('<style>body {position:relative} .widget_wrap{position:absolute;top:0;left:0;z-index:9999;padding:10px 20px;background:#222;border-bottom-right-radius:10px;-webkit-transition:all .3s ease;transition:all .3s ease;-webkit-transform:translate(-100%,0);-ms-transform:translate(-100%,0);transform:translate(-100%,0)}.widget_wrap:after{content:" ";position:absolute;top:0;left:100%;width:24px;height:24px;background:#222 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAABGdBTUEAALGPC/xhBQAAAAxQTFRF////////AAAA////BQBkwgAAAAN0Uk5TxMMAjAd+zwAAACNJREFUCNdjqP///y/DfyBg+LVq1Xoo8W8/CkFYAmwA0Kg/AFcANT5fe7l4AAAAAElFTkSuQmCC) no-repeat 50% 50%;cursor:pointer}.widget_wrap:hover{-webkit-transform:translate(0,0);-ms-transform:translate(0,0);transform:translate(0,0)}.widget_item{padding:0 0 10px}.widget_link{color:#fff;text-decoration:none;font-size:15px;}.widget_link:hover{text-decoration:underline} </style>');
+      widgetStilization.prependTo(".widget_wrap");
+    }
 
-    // pageWidget([
-    //   'index',
-    //   'buy-ticket',
-    //   'enter',
-    //   'personal',
-    //   'join',
-    //   'settings',
+    pageWidget([
+      'index',
+      'buy-ticket',
+      'enter',
+      'personal',
+      'join',
+      'settings',
 
-    // ]);
+    ]);
 
 
 
--- markup/blocks/popup/popup.js
+++ markup/blocks/popup/popup.js
@@ -1,1 +1,16 @@
+function popup () {
 
+  const $popupTrigger = $('.open-popup');
+
+   if ($popupTrigger.length > 0) {
+
+       $popupTrigger.magnificPopup({
+        type: 'inline'
+      });
+
+   }
+
+}
+
+
+module.exports = popup;
